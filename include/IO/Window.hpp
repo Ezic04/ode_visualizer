6e70e5a8117f32d42e78822ad072f659da515db8
JulianKonowalski <julian.konwalski@gmail.com>
2025-06-30 10:01:08 +0200

    update the Window.hpp file with new contents

--- include/IO/Window.hpp
+++ include/IO/Window.hpp
@@ -1,6 +1,8 @@
 #pragma once 
 
-namespace graphics {
+#include <string>
+
+namespace IO {
 
 /* 
  * @brief Wrapper around 
@@ -10,6 +12,18 @@ namespace graphics {
  */ 
 class Window {
 public:
+
+  /*
+   * @brief Structure for storing 
+   *  and passing around mouse 
+   *  state data.
+   */
+  struct MouseState {
+    float x_pos = 0.0f;
+    float y_pos = 0.0f;
+    float dx    = 0.0f;
+    float dy    = 0.0f;
+  };
 
   /*
    * @brief Creates a new window 
@@ -26,7 +40,7 @@ public:
    *  of any necessary initialization 
    *  steps.
    */
-  Window(int width, int height, const char* title);
+  Window(int width, int height, const std::string& title);
 
   /*
    * @brief Destroys the window 
@@ -63,9 +77,36 @@ public:
    */
   void pollEvents(void);
 
+  /*
+   * @brief Returns the array
+   *  of bools representing the
+   *  state of each key.
+   * 
+   * @return Array of bools representing
+   *  current state of all the keys.
+   */
+  const bool* const getKeyState(void) { return m_key_state; }
+
+  /*
+   * @brief Returns mouse state
+   *  data containing current mouse
+   *  position and change in position 
+   *  since the last mouse poll.
+   * 
+   * @return Mouse state data.
+   */
+  MouseState getMouseState(void);
+
 private:
 
+  void createCallbacks(void);
+  void handleMouseEvent(double x_pos, double y_pos);
+  void handleKeyboardEvent(int key, int action);
+
   void* m_window; //GLFWwindow* undercover
+  bool m_key_state[1024];
+  MouseState m_mouse_state;
+  bool m_mouse_first_moved;
   bool m_is_initialized;
 
 };
