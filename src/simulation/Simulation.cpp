b67d2da7e2ff87424aa603d9a2acb6d5047d7986
Slawek <slawek.tomasz.brzozka@gmail.com>
2025-08-17 16:02:48 +0200

    add graphics to simulaton

--- src/simulation/Simulation.cpp
+++ src/simulation/Simulation.cpp
@@ -2,17 +2,28 @@
 
 #include <cctype>
 #include <cmath>
-#include <cstddef>
 #include <iostream>
 #include <print>
 
+#include "IO/Window.hpp"
 #include "expr/expr.hpp"
+#include "graphics/Camera.hpp"
+#include "graphics/Mesh.hpp"
+#include "graphics/Program.hpp"
+#include "graphics/Vec3.hpp"
 #include "solver/solver.hpp"
 
-constexpr FloatType step = 0.01;
-constexpr int spu = 1 / step;
+namespace simulation {
 
-Simulation::Simulation(std::array<std::string, kDim> equations, std::string free_varible) {
+std::string shader_path = SHADER_PATH;
+
+std::vector<float> vertices = {-1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f};
+
+std::vector<unsigned int> indices = {0, 3, 1, 1, 3, 2, 2, 3, 0, 0, 1, 2};
+
+Simulation::Simulation(std::array<std::string, kDim> equations, std::string free_varible)
+    : is_running(false), time(0.0), win(800, 600, "ode_visualiser"), cam(graphics::Camera::Parameters()),
+      gpu_prog(shader_path + "/shader.vert", shader_path + "/shader.frag") {
   for (auto &eq : equations) {
     auto i = 0;
     while (std::isspace(eq[i])) { ++i; }
@@ -28,20 +39,50 @@ Simulation::Simulation(std::array<std::string, kDim> equations, std::string free
   }
   var_names.index_of(free_varible);
   // for (auto it : var_names.index_to_name) { std::cout << it; }
-  for (auto it : equations) { std::cout << it << '\n'; }
-  for (size_t i = 0; i < kDim; ++i) { system[i] = expr::parseExpr(equations[i], var_names); }
+  for (const auto &it : equations) { system.emplace_back(expr::parseExpr(it, var_names)); }
+}
 
-  FloatType x = 1.0;
-  FloatType t = 0.0;
-  FloatType x_euler, x_heun, x_rk4;
-  x_euler = x_heun = x_rk4 = x;
-  for (size_t i = 0; i <= spu * 10; ++i) {
-    if (i % spu == 0) {
-      std::println("t={}: x={}, x_euler={}, x_heun={}, x_rk4={}", t, std::exp(t), x_euler, x_heun, x_rk4);
+void Simulation::addEntity(graphics::Vec3 initial_position) {
+  entities.emplace_back(Mesh(vertices, indices, gpu_prog));
+  entities.back().setTranslation(initial_position);
+}
+
+void Simulation::run(const bool *simulate) {
+  int i = 0;
+  while (*simulate) {
+    if (is_running) {
+      for (auto &e : entities) {
+        win.clear();
+        win.pollEvents();
+        // if (!(++i % 100)) { std::println("t={}: x={},  x_rk4={}", time, std::sin(time), e.x); }
+        cam.render(e);
+        e.setTranslation(nextPosition(e.getTranslation()));
+        time += step;
+        win.swapBuffers();
+      }
+      // FloatType x = 1.0;
+      // FloatType t = 0.0;
+      // FloatType x_euler, x_heun, x_rk4;
+      // x_euler = x_heun = x_rk4 = x;
+      // for (size_t i = 0; i <= spu * 10; ++i) {
+      //   if (i % spu == 0) {
+      //     std::println("t={}: x={}, x_euler={}, x_heun={}, x_rk4={}", t, std::exp(t), x_euler, x_heun, x_rk4);
+      //     x_euler = solver::euler({system[0]}, {x_euler, t}, step).front();
+      //     x_heun = solver::heun({system[0]}, {x_heun, t}, step).front();
+      //     x_rk4 = solver::rk4({system[0]}, {x_rk4, t}, step).front();
+      //     t += step;
+      //   }
+      // }
     }
-    x_euler = solver::euler({system[0]}, {x_euler, t}, step).front();
-    x_heun = solver::heun({system[0]}, {x_heun, t}, step).front();
-    x_rk4 = solver::rk4({system[0]}, {x_rk4, t}, step).front();
-    t += step;
   }
 }
+
+Vec3 Simulation::nextPosition(const Vec3 &position) {
+  return toVec3(solver::rk4(system, {position.x, position.y, position.z, time}, step));
+}
+
+Vec3 Simulation::toVec3(const std::vector<FloatType> &vec) {
+  return {static_cast<float>(vec[0]), static_cast<float>(vec[1]), static_cast<float>(vec[2])};
+}
+
+} // namespace simulation
