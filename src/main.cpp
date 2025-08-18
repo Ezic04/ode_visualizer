e0c63aa5ec968af73ff2122706688ba611c5126b
Slawek <slawek.tomasz.brzozka@gmail.com>
2025-08-18 21:52:03 +0200

    update simulation class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
+#include "expr/expr.hpp"
 #include "simulation/Simulation.hpp"
+#include "solver/parser.hpp"
 
 int main() {
-  simulation::Simulation sim;
-  sim.addEntity({ 0.0f, 0.0f, 0.0f });
-  sim.run();
+  try {
+    auto [system, var_names] = solver::parseSystem({"x' = y", "y' = -x", "z' = 0"});
+    simulation::Simulation sim(system, var_names);
+    sim.addEntity({1.0f, 0.0f, 0.0f});
+    sim.run();
+  } catch (expr::ParserException e) { std::cout << e.what(); }
 }
--- src/simulation/Simulation.cpp
+++ src/simulation/Simulation.cpp
@@ -1,57 +1,62 @@
 #include "simulation/Simulation.hpp"
 #include "graphics/Mesh.hpp"
+#include <cstddef>
 
 using namespace simulation;
 
-Simulation::Simulation(
-  void
-) : m_window(800, 600, "ODE Visualizer"),
-    m_input_handler(&m_window),
-    m_camera(graphics::Camera::Parameters{
-      .position       = { -5.0f, 0.0f, 0.0f },
-      .screen_width   = 800,
-      .screen_height  = 600
-    }),
-    m_graphics_program(VERT_PATH, FRAG_PATH)
-{
-  m_input_handler.attachKeyCallback(87, [&](void) { m_camera.moveLongitudinal(0.001f); });
-  m_input_handler.attachKeyCallback(83, [&](void) { m_camera.moveLongitudinal(-0.001f); });
-  m_input_handler.attachKeyCallback(68, [&](void) { m_camera.moveLateral(0.001f); });
-  m_input_handler.attachKeyCallback(65, [&](void) { m_camera.moveLateral(-0.001); });
-  m_input_handler.attachKeyCallback(32, [&](void) { m_camera.moveVertical(0.001); });
-  m_input_handler.attachKeyCallback(341, [&](void) { m_camera.moveVertical(-0.001); });
-  m_input_handler.attachMouseCallback([&](const IO::Window::MouseState& mouse_state) {
-    m_camera.tiltPitch(mouse_state.dy * 0.1);
-    m_camera.tiltYaw(mouse_state.dx * 0.1);
-  }); 
+Simulation::Simulation(solver::System system, expr::VariableMap var_names)
+    : m_system(system), m_var_names(var_names), m_time(0), m_window(800, 600, "ODE Visualizer"),
+      m_input_handler(&m_window),
+      m_camera(graphics::Camera::Parameters{
+          .position = {0.0f, 0.0f, -5.0f}, .rotation{0, 90, 0}, .screen_width = 800, .screen_height = 600}),
+      m_graphics_program(VERT_PATH, FRAG_PATH) {
+  setupGrphics();
 }
 
 Simulation::~Simulation(void) {
 
   // free resources
+}
 
+void Simulation::setupGrphics() {
+  m_input_handler.attachKeyCallback(87, [&](void) { m_camera.moveLongitudinal(0.001f); });
+  m_input_handler.attachKeyCallback(83, [&](void) { m_camera.moveLongitudinal(-0.001f); });
+  m_input_handler.attachKeyCallback(68, [&](void) { m_camera.moveLateral(0.001f); });
+  m_input_handler.attachKeyCallback(65, [&](void) { m_camera.moveLateral(-0.001f); });
+  m_input_handler.attachKeyCallback(32, [&](void) { m_camera.moveVertical(0.001f); });
+  m_input_handler.attachKeyCallback(341, [&](void) { m_camera.moveVertical(-0.001f); });
+  m_input_handler.attachMouseCallback([&](const IO::Window::MouseState &mouse_state) {
+    m_camera.tiltPitch(mouse_state.dy * 0.1);
+    m_camera.tiltYaw(mouse_state.dx * 0.1);
+  });
 }
 
 void Simulation::run(void) {
-  while(!m_window.shouldClose()) {
+  while (!m_window.shouldClose()) {
     m_window.clear();
     m_window.pollEvents();
     m_input_handler.handleInputs();
-    for(auto& entity : m_entities) {
-      auto entity_position = entity.getTranslation();
+    for (size_t i = 0; i < m_entities.size(); ++i) {
+      m_positions[i] =
+          toVec3(solver::rk4(m_system, {m_positions[i].x, m_positions[i].y, m_positions[i].z, m_time}, step));
 
-      // simulation tick
+      m_entities[i].setTranslation(m_positions[i]);
 
-      m_camera.render(entity);
+      m_camera.render(m_entities[i]);
     }
     m_window.swapBuffers();
   }
 }
 
-void Simulation::addEntity(const graphics::Vec3& position) {
+void Simulation::addEntity(const graphics::Vec3 &position) {
   graphics::Mesh m = graphics::getDemoMesh(m_graphics_program);
   m.translate(position);
   m_entities.emplace_back(std::move(m));
+  m_positions.emplace_back(position);
+}
+
+graphics::Vec3 Simulation::toVec3(const std::vector<FloatType> &vec) {
+  return {static_cast<float>(vec[0]), static_cast<float>(vec[1]), static_cast<float>(vec[2])};
 }
 
 // std::vector<float> vertices = {-1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f};
@@ -61,22 +66,7 @@ void Simulation::addEntity(const graphics::Vec3& position) {
 // Simulation::Simulation(std::array<std::string, kDim> equations, std::string free_varible)
 //     : is_running(false), time(0.0), win(800, 600, "ode_visualiser"), cam(graphics::Camera::Parameters()),
 //       gpu_prog(shader_path + "/shader.vert", shader_path + "/shader.frag") {
-//   for (auto &eq : equations) {
-//     auto i = 0;
-//     while (std::isspace(eq[i])) { ++i; }
-//     auto begin = i;
-//     while (!std::isspace(eq[i])) { ++i; }
-//     auto end = i - 1;
-//     if (eq[end] != '\'') { throw expr::ParserException("lhs must be derivative of "); }
-//     while (std::isspace(eq[i])) { ++i; }
-//     if (eq[i] != '=') { throw expr::ParserException("varible name contains spaces or equal sign not found"); }
-//     std::string var_name = eq.substr(begin, end);
-//     var_names.index_of(var_name);
-//     eq = eq.substr(i + 1, eq.size());
-//   }
-//   var_names.index_of(free_varible);
-//   // for (auto it : var_names.index_to_name) { std::cout << it; }
-//   for (const auto &it : equations) { system.emplace_back(expr::parseExpr(it, var_names)); }
+
 // }
 
 // void Simulation::addEntity(graphics::Vec3 initial_position) {
@@ -116,8 +106,4 @@ void Simulation::addEntity(const graphics::Vec3& position) {
 
 // Vec3 Simulation::nextPosition(const Vec3 &position) {
 //   return toVec3(solver::rk4(system, {position.x, position.y, position.z, time}, step));
-// }
-
-// Vec3 Simulation::toVec3(const std::vector<FloatType> &vec) {
-//   return {static_cast<float>(vec[0]), static_cast<float>(vec[1]), static_cast<float>(vec[2])};
 // }
