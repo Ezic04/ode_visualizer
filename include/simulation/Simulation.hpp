82b94b503ab71a46d988b72806ffd12a40ec9a9c
Slawek <slawek.tomasz.brzozka@gmail.com>
2025-08-18 21:52:03 +0200

    update simulation class

--- include/simulation/Simulation.hpp
+++ include/simulation/Simulation.hpp
@@ -2,33 +2,43 @@
 
 #include <vector>
 
-#include "IO/Window.hpp"
 #include "IO/InputHandler.hpp"
+#include "IO/Window.hpp"
+#include "expr/expr.hpp"
 #include "graphics/Camera.hpp"
-#include "graphics/Program.hpp"
 #include "graphics/Mesh.hpp"
+#include "graphics/Program.hpp"
 #include "graphics/Vec3.hpp"
+#include "solver/solver.hpp"
+#include "utility/utility.hpp"
 
 namespace simulation {
 
+constexpr FloatType step = 0.01;
+
 class Simulation {
 public:
-
-  Simulation(void);
+  Simulation(solver::System system, expr::VariableMap var_names);
   ~Simulation(void);
 
   void run(void);
-  void addEntity(const graphics::Vec3& position);
+  void addEntity(const graphics::Vec3 &position);
 
 private:
+  void setupGrphics();
+  void updatePositions();
+  graphics::Vec3 toVec3(const std::vector<FloatType> &vec);
+
+  solver::System m_system;
+  expr::VariableMap m_var_names;
+  std::vector<graphics::Vec3> m_positions;
+  FloatType m_time;
 
   std::vector<graphics::Mesh> m_entities;
-
   IO::Window m_window;
   IO::InputHandler m_input_handler;
   graphics::Camera m_camera;
   graphics::Program m_graphics_program;
-
 };
 
-}
+} // namespace simulation
