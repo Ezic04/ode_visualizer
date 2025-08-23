caebfde695ece7d11d0fb2b4d673a4fd815e0334
JulianKonowalski <julian.konwalski@gmail.com>
2025-08-23 12:11:31 +0200

    update build system
    -remove .lib modules
    -remove unused dependencies (glm, glad, glfw)
    -comment out invalid simulation class code

--- include/simulation/Simulation.hpp
+++ include/simulation/Simulation.hpp
@@ -1,14 +1,6 @@
 #pragma once
 
-#include <vector>
-
-#include "IO/InputHandler.hpp"
-#include "IO/Window.hpp"
 #include "expr/expr.hpp"
-#include "graphics/Camera.hpp"
-#include "graphics/Mesh.hpp"
-#include "graphics/Program.hpp"
-#include "graphics/Vec3.hpp"
 #include "solver/solver.hpp"
 #include "utility/utility.hpp"
 
@@ -22,23 +14,18 @@ public:
   ~Simulation(void);
 
   void run(void);
-  void addEntity(const graphics::Vec3 &position);
+  // void addEntity(const graphics::Vec3 &position);
 
 private:
-  void setupGrphics();
+
   void updatePositions();
-  graphics::Vec3 toVec3(const std::vector<FloatType> &vec);
+  // graphics::Vec3 toVec3(const std::vector<FloatType> &vec);
 
   solver::System m_system;
   expr::VariableMap m_var_names;
-  std::vector<graphics::Vec3> m_positions;
+  // std::vector<graphics::Vec3> m_positions;
   FloatType m_time;
 
-  std::vector<graphics::Mesh> m_entities;
-  IO::Window m_window;
-  IO::InputHandler m_input_handler;
-  graphics::Camera m_camera;
-  graphics::Program m_graphics_program;
 };
 
-} // namespace simulation
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
-// #include "expr/expr.hpp"
-// #include "simulation/Simulation.hpp"
-// #include "solver/parser.hpp"
-
 #include <QApplication>
 
 #include "gui/MainWindow.hpp"
-
-// try {
-//   auto [system, var_names] = solver::parseSystem({"x' = y", "y' = -x", "z' = 0"});
-//   simulation::Simulation sim(system, var_names);
-//   sim.addEntity({1.0f, 0.0f, 0.0f});
-//   sim.run();
-// } catch (expr::ParserException e) { std::cout << e.what(); }
 
 int main(int argc, char **argv) {
   QGuiApplication app(argc, argv);
