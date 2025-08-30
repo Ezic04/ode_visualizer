240a418a2aefa031b98e57f374d013a075392530
JulianKonowalski <julian.konwalski@gmail.com>
2025-08-30 16:59:36 +0200

    merge simulation into main

--- include/gui/MainWindow.hpp
+++ include/gui/MainWindow.hpp
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <QMainWindow>
-#include <qobjectdefs.h>
+
+#include "gui/SimulationController.hpp"
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
-
 public:
 
   MainWindow(QWidget* parent = nullptr);
   ~MainWindow(void);
+
+private:
+
+  SimulationController m_simulation_controller;
 
 };
