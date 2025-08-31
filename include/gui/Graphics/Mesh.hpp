9ceeece9428a1f6bf5ea046a10b9827c9ff4bd8c
JulianKonowalski <julian.konwalski@gmail.com>
2025-08-31 12:40:37 +0200

    implement mesh instancing

--- include/gui/Graphics/Mesh.hpp
+++ include/gui/Graphics/Mesh.hpp
@@ -9,28 +9,35 @@ public:
 
   Mesh(void) = default;
   Mesh(const Mesh& other) = delete;
-  Mesh(Mesh&& other);
+  Mesh(Mesh&& other) = delete;
   ~Mesh(void);
 
   bool initializeGL(
     const std::vector<float>& vertices,
-    const std::vector<unsigned int>& indices
+    const std::vector<unsigned int>& indices,
+    const std::vector<float>& instances
   );
+
+  void updateInstances(const std::vector<float>& instances);
 
   inline const GLuint getVAO(void) const { return m_VAO; }
   inline const GLuint getIBO(void) const { return m_IBO; }
-  inline const GLuint getVBO(void) const { return m_VBO; }
+  inline const GLuint getVerticesVBO(void) const { return m_vertices_VBO; }
+  inline const GLuint getInstancesVBO(void) const { return m_instances_VBO; }
   
   inline const unsigned int getIndexCount(void) const { return m_index_count; }
   inline const unsigned int getVertexCount(void) const { return m_vertex_count; }
+  inline const unsigned int getInstanceCount(void) const { return m_instance_count; }
 
 protected:
 
   GLuint m_VAO = 0;
   GLuint m_IBO = 0;
-  GLuint m_VBO = 0;
+  GLuint m_vertices_VBO = 0;
+  GLuint m_instances_VBO = 0;
 
   unsigned int m_index_count = 0;
   unsigned int m_vertex_count = 0;
+  unsigned int m_instance_count = 0;
 
 };
