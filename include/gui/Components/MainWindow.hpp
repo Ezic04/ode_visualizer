bcdbebc73d6d2c738f829a12acccdf2a8a4876aa
JulianKonowalski <julian.konwalski@gmail.com>
2025-09-01 17:30:21 +0200

    tidy up Main Window class constructor

--- include/gui/Components/MainWindow.hpp
+++ include/gui/Components/MainWindow.hpp
@@ -2,9 +2,6 @@
 
 #include <QMainWindow>
 
-#include <QShortcut>
-#include <qkeysequence.h>
-
 #include "gui/Components/SimulationController.hpp"
 
 class MainWindow : public QMainWindow {
@@ -14,7 +11,5 @@ public:
   ~MainWindow(void);
 
 private:
-  QShortcut* m_show_controls_shortcut = nullptr;
-
-  SimulationController *m_simulation_controller;
+  SimulationController* m_simulation_controller;
 };
--- src/gui/Components/MainWindow.cpp
+++ src/gui/Components/MainWindow.cpp
@@ -1,52 +1,47 @@
 #include "gui/Components/MainWindow.hpp"
 
-#include <stdexcept>
-
 #include <QWidget>
-#include <QHBoxLayout>
-#include <QPushButton>
+#include <QShortcut>
 #include <QDockWidget>
 #include <QSurfaceFormat>
 
 #include "gui/Components/Viewport.hpp"
 #include "gui/Components/ControlPanel.hpp"
-#include "gui/Components/SimulationController.hpp"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+MainWindow::MainWindow(
+  QWidget *parent
+) : QMainWindow(parent), 
+    m_simulation_controller(new SimulationController)
+{
+  // disable vsync globally 
   QSurfaceFormat format;
   format.setSwapInterval(0);
   QSurfaceFormat::setDefaultFormat(format);
 
-  auto viewport = new Viewport;
-  auto control_panel = new ControlPanel;
-  auto dockable_container = new QDockWidget("Control Panel", this);
-  
-  m_simulation_controller = new SimulationController;
+  auto* viewport = new Viewport;
+  auto* control_panel = new ControlPanel;
+  auto* dockable_container = new QDockWidget("Control Panel", this);
 
-  if (!viewport) { throw std::runtime_error("Failed to allocate the Viewport component"); }
-  if (!control_panel || !dockable_container) { throw std::runtime_error("Failed to allocate the Control Panel component"); }
-  if (!m_simulation_controller) { throw std::runtime_error("Failed to allocate the Simulation Controller component"); }
-
+  // widget setup
   dockable_container->setWidget(control_panel);
   dockable_container->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
 
+  // inter-panel communication
   connect(m_simulation_controller, &SimulationController::simulationUpdated, viewport, &Viewport::renderFrame);
   connect(viewport, &Viewport::frameFinished, m_simulation_controller, &SimulationController::updateSimulation);
-  connect(control_panel, &ControlPanel::equationsChanged, m_simulation_controller,
-          &SimulationController::updateEquations);
+  connect(control_panel, &ControlPanel::equationsChanged, m_simulation_controller, &SimulationController::updateEquations);
 
-  this->setCentralWidget(QWidget::createWindowContainer(viewport));
-  this->addDockWidget(Qt::LeftDockWidgetArea, dockable_container);
-  this->setMinimumSize(640, 360);
-  this->resize(1280, 720);
-
-  m_show_controls_shortcut = new QShortcut(QKeySequence(Qt::Key_C), this);
-  connect(m_show_controls_shortcut, &QShortcut::activated, this, [=](){
+  // shortcuts
+  connect(new QShortcut(QKeySequence(Qt::Key_C), this), &QShortcut::activated, this, [=](){
     if (!dockable_container->isVisible()) { dockable_container->show(); } 
     else { dockable_container->hide(); }
   });
+
+  // main window properties
+  this->resize(1280, 720);
+  this->setMinimumSize(640, 360);
+  this->addDockWidget(Qt::LeftDockWidgetArea, dockable_container);
+  this->setCentralWidget(QWidget::createWindowContainer(viewport));
 }
 
-MainWindow::~MainWindow(void) {
-  delete m_simulation_controller;
-}
+MainWindow::~MainWindow(void) {}
