5e052518f0fcf0422e8afedeb9f33604cfff2342
JulianKonowalski <julian.konwalski@gmail.com>
2025-09-05 13:49:14 +0200

    implement equation textbox resizing

--- include/gui/Components/EquationsList.hpp
+++ include/gui/Components/EquationsList.hpp
@@ -4,34 +4,56 @@
 #include <QTextEdit>
 #include <QListWidget>
 
-class EquationTextBox : public QTextEdit{
-  Q_OBJECT
-public:
-
-  EquationTextBox(QWidget* parent = nullptr);
-
-  virtual QSize sizeHint(void) const override;
-
-protected:
-  virtual void resizeEvent(QResizeEvent* event) override;
-
-private:
-
-};
+/*****************/
+/* EQUATION LIST */
+/*****************/
 
 class EquationsList : public QWidget {
   Q_OBJECT
 public:
 
   EquationsList(QWidget* parent = nullptr);
- 
+  ~EquationsList(void);
+
 public slots:
 
   void addEquation(void);
   void removeEquation(void);
 
-private:
+signals:
+  void resized(const QSize& new_viewport_size);
 
+protected:
+  virtual void resizeEvent(QResizeEvent* event) override;
+
+private:
   QListWidget* m_list = nullptr;
+
+};
+
+/************/
+/* TEXT BOX */
+/************/
+
+class EquationTextBox : public QTextEdit{
+  Q_OBJECT
+public:
+
+  EquationTextBox(QWidget* parent = nullptr);
+  ~EquationTextBox(void) = default;
+
+  virtual QSize sizeHint(void) const override;
+
+public slots:
+  void onParentResize(const QSize& new_vieport_size);
+
+signals:
+  void resized(const QSize& new_size);
+
+protected:
+  virtual void resizeEvent(QResizeEvent* event) override;
+
+private slots:
+  void onTextChanged(void);
 
 };
