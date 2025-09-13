bd16d35932fdcc2a217d61cfb70be9e9d7f02ed4
JulianKonowalski <julian.konwalski@gmail.com>
2025-09-13 19:54:43 +0200

    improved grid shader

--- include/gui/Graphics/WorldGrid.hpp
+++ include/gui/Graphics/WorldGrid.hpp
@@ -8,16 +8,23 @@
 
 class WorldGrid : public Mesh {
 public:
+
   WorldGrid(void);
   ~WorldGrid(void);
   
-  void draw(const QVector3D& camera_position, const QMatrix4x4& camera_matrix);
+  void draw(
+    const QMatrix4x4& camera_matrix,
+    const QVector3D& camera_position,
+    const QVector3D& focus_point_position
+  );
 
 private:
 
   OpenGLFunctions* m_gl = nullptr;
   QOpenGLShaderProgram* m_program = nullptr;
-  GLuint m_camera_position_uniform = 0;
+
   GLuint m_camera_matrix_uniform = 0;
+  GLuint m_camera_position_uniform = 0;
+  GLuint m_focus_point_position_uniform = 0;
   
 };
--- src/gui/Graphics/WorldGrid.cpp
+++ src/gui/Graphics/WorldGrid.cpp
@@ -16,9 +16,10 @@ WorldGrid::WorldGrid(
   m_program->addShaderFromSourceFile(QOpenGLShader::Fragment, frag_path.c_str());
   m_program->link();
   m_program->bind();
-  
-  m_camera_position_uniform = m_program->uniformLocation("camera_position");
+ 
   m_camera_matrix_uniform = m_program->uniformLocation("camera_matrix");
+  m_camera_position_uniform = m_program->uniformLocation("camera_position");
+  m_focus_point_position_uniform = m_program->uniformLocation("focus_point_position");
 
   m_program->release();
 }
@@ -28,15 +29,17 @@ WorldGrid::~WorldGrid(void) {
 }
 
 void WorldGrid::draw(
-  const QVector3D& camera_position, 
-  const QMatrix4x4& camera_matrix
+  const QMatrix4x4& camera_matrix,
+  const QVector3D& camera_position,
+  const QVector3D& focus_point_position
 ) {
   m_program->bind();
   m_gl->glBindVertexArray(m_VAO);
   m_gl->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
 
-  m_program->setUniformValue(m_camera_position_uniform, camera_position);
   m_program->setUniformValue(m_camera_matrix_uniform, camera_matrix);
+  m_program->setUniformValue(m_camera_position_uniform, camera_position);
+  m_program->setUniformValue(m_focus_point_position_uniform, focus_point_position);
 
   m_gl->glDepthMask(GL_FALSE);
   m_gl->glDrawElements(GL_TRIANGLES, m_index_count, GL_UNSIGNED_INT, nullptr);
