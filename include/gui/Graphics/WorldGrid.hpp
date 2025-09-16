b7e128faad6bba8ec65860f5df826c5f9f776cc4
Julian Konowalski <julian.konwalski@gmail.com>
2025-09-16 10:44:36 +0200

    parametrize world grid shader

--- include/gui/Graphics/WorldGrid.hpp
+++ include/gui/Graphics/WorldGrid.hpp
@@ -6,12 +6,30 @@
 #include "gui/graphics/Mesh.hpp"
 #include "gui/graphics/OpenGLFunctions.hpp"
 
+struct GridParameters {
+  float cell_size = 1.0f;
+  float fade_start_distance = 10.0f;
+  float fade_end_distance = 30.0f;
+  QVector3D grid_color = { 0.29f, 0.29f, 0.29f };
+  QVector3D x_axis_color = { 1.0f, 0.0f, 0.0f };
+  QVector3D z_axis_color = { 0.0f, 0.0f, 1.0f };
+};
+
 class WorldGrid : public Plane {
 public:
 
-  WorldGrid(void);
+  WorldGrid(const GridParameters& params = GridParameters());
   ~WorldGrid(void);
   
+  void setParameters(const GridParameters& params);
+
+  void setCellSize(float cell_size);
+  void setFadeStartDistance(float fade_start_distance);
+  void setFadeEndDistance(float fade_end_distance);
+  void setGridColor(const QVector3D& grid_color);
+  void setXAxisColor(const QVector3D& x_axis_color);
+  void setZAxisColor(const QVector3D& z_axis_color);
+
   void draw(
     const QMatrix4x4& camera_matrix,
     const QVector3D& camera_position,
@@ -25,6 +43,12 @@ private:
 
   GLuint m_camera_matrix_uniform = 0;
   GLuint m_camera_position_uniform = 0;
-  GLuint m_focus_point_position_uniform = 0;
   
+  GLuint m_cell_size_uniform = 0;
+  GLuint m_fade_start_uniform = 0;
+  GLuint m_fade_end_uniform = 0;
+  GLuint m_grid_color_uniform = 0;
+  GLuint m_x_axis_color_uniform = 0;
+  GLuint m_z_axis_color_uniform = 0;
+
 };
